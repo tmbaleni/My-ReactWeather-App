a8928d9d167f7f7b9315cbbfcca324c66929fac5
Tabo Mbaleni <Tabo Mbaleni>
2017-08-03 10:12:48 +0200

    Styled About page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,23 @@ var React  = require('react');
 
 var About = () => {
 		return (
-			<h3>About Component</h3>
+			<div>
+			<h1 className="text-center">About</h1>
+			<p>This is a weather application built on React. I built this for The Complete React Web App Development Course.</p>
+			<p>
+				Here are some of the tools I used:
+			</p>
+			<ul>
+				<li>
+					<a href="https://facebook.github.io/react">React</a> -
+					This was the JavaScript framework used.
+				</li>
+				<li>
+					<a href="http://openweathermap.org">Open Weather Map</a> -
+					I used Open Weather Map to search for weather data by city name.
+				</li>
+			</ul>
+			</div>
 		)
 	};
 
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-vvar express = require('express');
+var express = require('express');
 
 // Create our app
 var app = express();
